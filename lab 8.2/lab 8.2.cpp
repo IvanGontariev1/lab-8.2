cb35b9dc9ddfee65bd60633cd33aac71a405e477
grush <grush@DESKTOP-JFK81GP>
2024-06-25 23:40:22 +0300

    Добавьте файлы проекта.

---
+++ lab 8.2/lab 8.2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+
+int main() {
+
+    const int n = 5;
+    double array[n] = { 1.5, -2.3, 3.7, -4.5, 5.1 };
+
+
+    double sum_positive = 0;
+    double product = 1;
+
+
+    for (int i = 0; i < n; ++i) {
+        if (array[i] > 0) {
+            sum_positive += array[i];
+        }
+        product *= array[i];
+    }
+
+
+    std::cout << "Sum of positive elements: " << sum_positive << std::endl;
+    std::cout << "Product of all elements: " << product << std::endl;
+
+    return 0;
+}
